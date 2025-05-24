5182af6e00311c46844688afff2ae7b96e171601
Makiese Morais <makiesse.dev@gmail.com>
2025-05-24 12:09:57 +0100

    more implementation

--- mod00/ex02/Account.cpp
+++ mod00/ex02/Account.cpp
@@ -8,19 +8,20 @@ int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit) {
-	//this._accountIndex = 0;
-	//this._amount = initial_deposit;	
-	
-	std::cout << "Construcor\n";
+	this->_accountIndex = Account::_nbAccounts++;
+	this->_amount = initial_deposit;	
+	std::cout << Account::_displayTimestamp() << " ";	
+	std::cout << "index:" + this->_accountIndex << ";";
+	std::cout << "amount:" + this->_amount << ";";
+	std::cout << "created" << std::endl;
 }
 
 void Account::_displayTimestamp() {
-	/*std::time_t now = std::time(0);
+	std::time_t now = std::time(0);
 	std::tm* utc_time = std::gmtime(&now);
 	char ftime[80];
 	std::strftime(ftime, sizeof(ftime), "[%Y%m%d_%H%M%S]");
-	std::cout << ftime */
-	std::cout << "_displayTimestamp\n";
+	std::cout << ftime;
 } 
 
 int	Account::getNbAccounts( void ) {
